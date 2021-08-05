a9dae6fbea769f591f9972be1e165f17a0f8a238
maltenorstroem <malte.norstroem@gmail.com>
2021-08-05 11:36:58 +0200

    feat: furo/ui5 package integrated

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Furo meets Vue App"/>
+    <HelloWorld msg="Welcome to Vue meets Eclipse Furo"/>
   </div>
 </template>
 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,21 +9,26 @@
       <li><a href="https://www.npmjs.com/package/@furo/framework" target="_blank" rel="noopener">@furo/framework</a>
       </li>
       <li><a href="https://www.npmjs.com/package/@furo/data" target="_blank" rel="noopener">@furo/data</a></li>
+      <li><a href="https://www.npmjs.com/package/@furo/ui5" target="_blank" rel="noopener">@furo/ui5</a></li>
     </ul>
-    <h3>Integrated Furo Services</h3>
-    <div>
-      <ul>
-        <li v-for="(value, name) in services" v-bind:key="name">
-        {{ name }}: {{ value }}
-        </li>
-      </ul>
-    </div>
+    <furo-ui5-card heading="Integrated Furo Services" subheading="API Services">
+      <furo-ui5-button slot="action">Start</furo-ui5-button>
+      <div slot="content">
+        <ul>
+          <li v-for="(value, name) in services" v-bind:key="name">
+            {{ name }}: {{ value }}
+          </li>
+        </ul>
+      </div>
+    </furo-ui5-card>
 
   </div>
 </template>
 
 <script>
 import {Env} from '@furo/framework';
+import '@furo/ui5/src/furo-ui5-card.js';
+import '@furo/ui5/src/furo-ui5-button.js';
 
 export default {
   name: 'HelloWorld',
