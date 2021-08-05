acbe14c68e4081c6e81df9fe76d1a7f6a8853420
maltenorstroem <malte.norstroem@gmail.com>
2021-08-05 11:54:25 +0200

    feat: sap ui5/fiori package integrated

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <ui5-shellbar
+        primary-title="VueJS meets Eclipse Furo"
+        secondary-title="Integration Example"
+    ></ui5-shellbar>
     <HelloWorld msg="Welcome to Vue meets Eclipse Furo"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import '@ui5/webcomponents-fiori/dist/ShellBar.js';
 
 export default {
   name: 'App',
@@ -17,20 +21,15 @@ export default {
 </script>
 
 <style>
+body {
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  --split-master-width: 420px;
 }
-img {
-  transform: rotate(
-      180deg
-  );
-  filter: hue-rotate(
-      56deg
-  );
-}
+
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,20 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      Here we want to show a simple example how to integrate the fantastic Eclipse Furo Stack into a Vue application.
-    </p>
-    <h3>Used Packages</h3>
-    <ul>
-      <li><a href="https://www.npmjs.com/package/@furo/framework" target="_blank" rel="noopener">@furo/framework</a>
-      </li>
-      <li><a href="https://www.npmjs.com/package/@furo/data" target="_blank" rel="noopener">@furo/data</a></li>
-      <li><a href="https://www.npmjs.com/package/@furo/ui5" target="_blank" rel="noopener">@furo/ui5</a></li>
-    </ul>
+  <furo-split-view>
+    <div slot="master" class="center">
+      <img alt="Vue logo" src="../assets/logo.png">
+      <h1>{{ msg }}</h1>
+      <p>
+        Here we want to show a simple example how to integrate the fantastic Eclipse Furo Stack into a Vue application.
+      </p>
+      <h3>Used Packages</h3>
+      <ul>
+        <li><a href="https://www.npmjs.com/package/@furo/framework" target="_blank" rel="noopener">@furo/framework</a>
+        </li>
+        <li><a href="https://www.npmjs.com/package/@furo/data" target="_blank" rel="noopener">@furo/data</a></li>
+        <li><a href="https://www.npmjs.com/package/@furo/ui5" target="_blank" rel="noopener">@furo/ui5</a></li>
+      </ul>
+    </div>
+
     <furo-ui5-card heading="Integrated Furo Services" subheading="API Services">
       <furo-ui5-button slot="action">Start</furo-ui5-button>
       <div slot="content">
@@ -22,11 +26,13 @@
       </div>
     </furo-ui5-card>
 
-  </div>
+  </furo-split-view>
+
 </template>
 
 <script>
 import {Env} from '@furo/framework';
+import '@furo/layout/src/furo-split-view.js';
 import '@furo/ui5/src/furo-ui5-card.js';
 import '@furo/ui5/src/furo-ui5-button.js';
 
@@ -45,10 +51,20 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+furo-split-view {
+  padding: 0.25rem 1rem;
+}
 h3 {
   margin: 40px 0 0;
 }
-
+img {
+  transform: rotate(
+      180deg
+  );
+  filter: hue-rotate(
+      56deg
+  );
+}
 ul {
   list-style-type: none;
   padding: 0;
@@ -61,5 +77,8 @@ li {
 
 a {
   color: #504163;
+}
+.center{
+  text-align: center;
 }
 </style>
